1db8caf74c54a8584d018842b21198aa2289cff5
Winston Weinert <winston@ml1.net>
2014-04-06 14:28:32 -0500

    Fix openssl invocation to work on Macs. Tunable openssl variable.
    - Ignoring *.zip files
    - in the openssl dgst ... -verify do NOT use -sha1, use -dss1 for
      reproducable results across platforms.
    - set VERIFY_WITH_OPENSSL in environment to configure the openssl(1)
      path.

--- _verify_sparkle_signature.sh
+++ _verify_sparkle_signature.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-OPENSSL='/usr/bin/openssl'
+: ${VERIFY_WITH_OPENSSL:='/usr/bin/openssl'}
 DSAPUBKEY="$1"
 SIGNATURE="$2"
 ZIPFILE="$3"
@@ -13,11 +13,9 @@ my_mktemp(){
     mktemp -t "${MKTEMP_TEMPLATE}${1}"
 }
 
-DECODED_SIGNATURE_FILE="$(my_mktemp sigfile)"
-ZIPFILE_SHA1_FILE="$(my_mktemp zipfile_sha1)"
-
 usage() {
     printf '%s DSAPUBKEY SIGNATURE ZIPFILE\n' "$0"
+    echo 'Override environment variable VERIFY_WITH_OPENSSL to use different OpenSSL.'
     echo
     printf 'Example: %s public_dsa.pem "MCwCFGRnB0iQO97Nzf2Jaq1WIWh1Jym0AhRhfxNTjunEtMxar8naY5wEBvvEow==" my-app.zip\n' "$0"
     exit
@@ -27,8 +25,11 @@ if [ $# != 3 ]; then
     usage
 fi
 
-echo "$SIGNATURE" | "$OPENSSL" enc -base64 -d > "$DECODED_SIGNATURE_FILE"
-"$OPENSSL" dgst -sha1 -binary < "$ZIPFILE" > "$ZIPFILE_SHA1_FILE"
-openssl dgst -sha1 -verify "$DSAPUBKEY" -signature "$DECODED_SIGNATURE_FILE" "$ZIPFILE_SHA1_FILE"
+DECODED_SIGNATURE_FILE="$(my_mktemp sigfile)"
+ZIPFILE_SHA1_FILE="$(my_mktemp zipfile_sha1)"
+
+echo "$SIGNATURE" | "$VERIFY_WITH_OPENSSL" enc -base64 -d > "$DECODED_SIGNATURE_FILE"
+"$VERIFY_WITH_OPENSSL" dgst -sha1 -binary < "$ZIPFILE" > "$ZIPFILE_SHA1_FILE"
+"$VERIFY_WITH_OPENSSL" dgst -dss1 -verify "$DSAPUBKEY" -signature "$DECODED_SIGNATURE_FILE" "$ZIPFILE_SHA1_FILE"
 
 rm -f "$DECODED_SIGNATURE_FILE" "$ZIPFILE_SHA1_FILE"
